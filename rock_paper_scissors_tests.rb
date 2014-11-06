c61d1ea3a21fb7b8a143a7da3a078a5ce9a6a400
trikitrok <trikitrok@gmail.com>
2014-11-06 19:17:06 +0100

    Refactor: reduced duplication in tests

--- rock_paper_scissors_tests.rb
+++ rock_paper_scissors_tests.rb
@@ -2,15 +2,19 @@ require File.join(File.dirname(__FILE__), "rock_paper_scissors")
 require 'test/unit'
 
 class TestRockPaperScissors < Test::Unit::TestCase
+
+  def setup
+    @game = Game.new
+    @paper = Paper.new
+  end
+
   def test_paper_against_paper
-      game = Game.new
-      result = game.hand(Paper.new, Paper.new)
+      result = @game.hand(@paper, Paper.new)
       assert_equal(result.to_s, "Two Papers, no one wins")
   end
 
   def test_paper_against_rock
-    sut = Game.new
-    result = sut.hand(Paper.new, Rock.new)
+    result = @game.hand(@paper, Rock.new)
     assert_equal(result.to_s, "Paper wins")
   end
 end
